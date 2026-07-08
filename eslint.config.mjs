f560d1ce14b31f0082df0c8e946b629bcf966e02
mts88 <fsca.motisi@gmail.com>
2026-07-08 19:07:33 +0200

    build: fail lint on warnings and ignore the yarn folder explicitly

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist/", "esbuild.js"] },
+  { ignores: ["dist/", ".yarn/", "esbuild.js"] },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
